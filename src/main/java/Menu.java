cba110af99df0c3da05b60926e86eb3f752b642f
fredericohubler <fredericohubler2@gmail.com>
2017-10-27 17:27:28 -0200

    Adicionei o método remove usuario e coloquei ele no menu.

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -12,27 +12,33 @@ public class Menu {
         System.out.println("Selecione a opção desejada:");
         System.out.println("1 - Entrar com usuário já existente.");
         System.out.println("2 - Criar novo usuário.");
-        String in = sc.next();
-        while (!(in.equals("1") || in.equals("2"))){
+        System.out.println("3 - Remover usuário existente.");
+        String in = sc.nextLine();
+        while (!(in.equals("1") || in.equals("2") || in.equals("3"))){
             System.out.println("Opção invalida.");
             System.out.println("Selecione a opção desejada:");
             System.out.println("1 - Entrar com usuário já existente.");
             System.out.println("2 - Criar novo usuário.");
-            in=sc.next();
+            System.out.println("3 - Remover usuário existente.");
+            in=sc.nextLine();
         }
-        if(in.equals("1")){
-            if(dbdao.retornaCpfUsers().size()==0){
-                System.out.println("Voce não tem nenhum usuario criado. Retornando ao menu inicial.");
-                menuInicial();
-                return;
-            }
-            selecionaUser();
-            return;
+        switch (Integer.parseInt(in)){
+            case 1: if(dbdao.retornaCpfUsers().size()==0){
+                        System.out.println("Voce não tem nenhum usuario criado. Retornando ao menu inicial.");
+                        menuInicial();
+                        return;
+                    }
+                    menuContas(selecionaUser());
+                    return;
+
+            case 2: criaUser();
+                    return;
+
+            case 3: deletarUser();
+                    return;
         }
-        else{
-            criaUser();
-            return;
-        }
+
+
     }
 
     public void criaUser(){
@@ -41,9 +47,9 @@ public class Menu {
         String cpf = "";
         while(true){//Aqui pega o nome do usuario e pede para confirmar se está certo.
             System.out.println("Digite o nome do novo usuário:");
-            nome=sc.next();
+            nome=sc.nextLine();
             System.out.println("Nome: "+nome+"\nPara confirmar digite 'sim'");
-            String confirm=sc.next();
+            String confirm=sc.nextLine();
             if(confirm.equalsIgnoreCase("sim")){
                 break;
             }
@@ -51,19 +57,19 @@ public class Menu {
 
         while(true){//Aqui pega o CPF do usuario e pede para confirmar se está certo.
             System.out.println("Digite o CPF do novo usuário:");
-            cpf=sc.next();
+            cpf=sc.nextLine();
             while(!cpf.matches("[0-9]+")){//Aqui testa se o CPF é composto apenas de numeros.
                 System.out.println("O CPF deve conter somente numeros.");
                 System.out.println("Digite o CPF do novo usuário:");
-                cpf=sc.next();
+                cpf=sc.nextLine();
             }
             while(!(cpf.length()==11)){
                 System.out.println("O CPF deve conter exatamente 11 numeros.");
                 System.out.println("Digite o CPF do novo usuário:");
-                cpf=sc.next();
+                cpf=sc.nextLine();
             }
             System.out.println("CPF: "+cpf+"\nPara confirmar digite 'sim'");
-            String confirm=sc.next();
+            String confirm=sc.nextLine();
             if(confirm.equalsIgnoreCase("sim")){
                 break;
             }
@@ -83,21 +89,72 @@ public class Menu {
         ArrayList<String> nomes = dbdao.retornaNomeUsers();
         System.out.println("Selecione um usuario:");
         for (i = 0; i < cpfs.size(); i++) {
-            System.out.println("1 - Nome: "+nomes.get(i)+" CPF:"+cpfs.get(i));
+            System.out.println((i+1)+" - Nome: "+nomes.get(i)+" CPF:"+cpfs.get(i));
         }
-        String in=sc.next();
+        String in=sc.nextLine();
         while(true){
             if(in.matches("[0-9]+")){//Checa se a string tem apenas numeros.
                 if(Integer.parseInt(in)<=cpfs.size()){//Checa se o numero é valido.
-                    retorno = new User(nomes.get(Integer.parseInt(in)), cpfs.get(Integer.parseInt(in)));//Cria um user com os valores do indice.
+                    retorno = new User(nomes.get(Integer.parseInt(in)-1), cpfs.get(Integer.parseInt(in)-1));//Cria um user com os valores do indice.
                     break;
                 }
-                else System.out.println("Não existe um usuario com este numero. Digite novamente.");in=sc.next();
+                else{
+                    System.out.println("Não existe um usuario com este numero. Digite novamente.");
+                    in=sc.nextLine();
+                }
+
+            }
+            else System.out.println("Digite apenas numeros.");in=sc.nextLine();
+        }
+        return retorno;
+    }
+
+    public void deletarUser(){
+        DatabaseDAO dbdao = new DatabaseDAO();
+        UserDAO userDAO = new UserDAO();
+        User deletado = null;
+
+        ArrayList<String> cpfs = dbdao.retornaCpfUsers();
+        ArrayList<String> nomes = dbdao.retornaNomeUsers();
+        System.out.println("Selecione qual usuario será deletado:");
+        for (int i = 0; i < cpfs.size(); i++) {
+            System.out.println((i+1)+" - Nome: "+nomes.get(i)+" CPF:"+cpfs.get(i));
+        }
+        String in=sc.nextLine();
+        while(true){
+            if(in.matches("[0-9]+")){//Checa se a string tem apenas numeros.
+                if(Integer.parseInt(in)<=cpfs.size()){//Checa se o numero é valido.
+                    deletado = new User(nomes.get(Integer.parseInt(in)-1), cpfs.get(Integer.parseInt(in)-1));//Cria um user com os valores do indice.
+                    break;
+                }
+                else{
+                    System.out.println("Não existe um usuario com este numero. Digite novamente.");
+                    in=sc.nextLine();
+                }
 
             }
             else System.out.println("Digite apenas numeros.");in=sc.next();
-
         }
-        return retorno;
+        while (true){
+            System.out.println("Para confirmar a exclusão do usuario "+deletado.getNome()+", digite o nome do usuario a ser deletado:");
+            in=sc.next();
+            if(in.equals(deletado.getNome())){
+                userDAO.remove(deletado);
+                System.out.println("Usuario "+deletado.getNome()+" excluido com sucesso.");
+                menuInicial();
+                return;
+            }
+            else {
+                System.out.println("Nome incorreto. Voltando para o menu inicial.");
+                menuInicial();
+                return;
+            }
+        }
+    }
+
+    public void menuContas(User usuarioSelecionado){
+        UserDAO userDAO = new UserDAO();
+        String printzao = userDAO.retornarNome(usuarioSelecionado) + "   " + userDAO.retornarCPF(usuarioSelecionado);
+        System.out.println(printzao);
     }
 }
--- src/main/java/UserDAO.java
+++ src/main/java/UserDAO.java
@@ -1,4 +1,5 @@
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDAO {
@@ -26,6 +27,34 @@ public class UserDAO {
             prep.execute();
             prep.close();
             return true;
+        } catch (SQLException u){
+            System.out.println(u);
+            throw new RuntimeException(u);
+        }
+    }
+
+    public String retornarNome(User user){
+        String insert="SELECT Nome FROM User WHERE CPF = ?";
+        try{
+            PreparedStatement prep = Database.getConexaoMySQL().prepareStatement(insert);
+            prep.setString(1,user.getCPF());
+            ResultSet st = prep.executeQuery();
+            st.next();
+            return st.getString("Nome");
+        } catch (SQLException u){
+            System.out.println(u);
+            throw new RuntimeException(u);
+        }
+    }
+
+    public String retornarCPF(User user){
+        String insert="SELECT CPF FROM User WHERE CPF = ?";
+        try{
+            PreparedStatement prep = Database.getConexaoMySQL().prepareStatement(insert);
+            prep.setString(1,user.getCPF());
+            ResultSet st = prep.executeQuery();
+            st.next();
+            return st.getString("CPF");
         } catch (SQLException u){
             System.out.println(u);
             throw new RuntimeException(u);
